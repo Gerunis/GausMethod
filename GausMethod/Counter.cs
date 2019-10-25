54d0b07fd45ebf102a8b29cad7147ca3971b6d9d
Сергей Русинов <fftron@me.com>
2019-10-25 19:52:14 +0500

    Метод прогонки

--- GausMethod/Counter.cs
+++ GausMethod/Counter.cs
@@ -53,7 +53,7 @@ namespace GausMethod
             var res = new double[array.GetLength(0)];
 
             for (var i = 0; i < array.GetLength(0); i++)
-                Console.WriteLine("x{0} = {1}",i + 1, array[i, array.GetLength(1) - 1] / array[i, i]);
+                Console.WriteLine("x{0} = {1:N5}",i + 1, array[i, array.GetLength(1) - 1] / array[i, i]);
 
             //foreach (var i in res) Console.Write(i + " ");
             return res;
@@ -69,6 +69,55 @@ namespace GausMethod
                 array[b, i] = k;
             }
 
+        }
+
+        public static void ThomasMethod(double[,] array)
+        {
+            int n = array.GetLength(0);
+            var e = new Dictionary<char, double>[n];
+            for(var i = 0; i < n; i++)
+            {
+                e[i] = new Dictionary<char, double>();
+                e[i]['a'] = i - 1 >= 0 ? array[i, i - 1] : 0;
+                e[i]['b'] = array[i, i];
+                e[i]['c'] = i + 1 < n ? array[i, i + 1] : 0;
+                e[i]['d'] = array[i, n];
+                e[i]['u'] = 0;
+                e[i]['v'] = 0;
+                e[i]['x'] = 0;
+            }
+
+            e[0]['u'] = -e[0]['c'] / e[0]['b'];
+            e[0]['v'] = e[0]['d'] / e[0]['b'];
+            //e[i]['']
+
+            for (var i = 1; i < n - 1; i++)
+            {
+                e[i]['u'] = -e[i]['c'] /(e[i]['a']* e[i-1]['u'] + e[i]['b']);
+                e[i]['v'] = (e[i]['d'] - e[i]['a']* e[i-1]['v']) / (e[i]['a'] * e[i - 1]['u'] + e[i]['b']);
+            }
+
+            e[n-1]['x'] = (e[n-1]['d'] - e[n-1]['a'] * e[n - 2]['v']) / (e[n-1]['a'] * e[n - 2]['u'] + e[n-1]['b']);
+
+            for (var i = 1; i < n; i++)
+            {
+                e[n - 1 - i]['x'] = e[n - i]['x'] * e[n - 1 - i]['u'] + e[n - 1 - i]['v'];
+            }
+            DrawTable(e);
+
+            for (var i = 0; i < n; i++)
+                Console.WriteLine("x{0} = {1:N5}", i + 1, e[i]['x']);
+
+        }
+
+        public static void DrawTable(Dictionary<char,double>[] table)
+        {
+            Console.WriteLine("i  a        b        c         d         u         v       x");
+            for(var i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine("{0}  {1:N5}  {2:N5}  {3:N5}  {4:N5}  {5:N5}  {6:N5}  {7:N5}",i, table[i]['a'], table[i]['b'], table[i]['c'], table[i]['d'], table[i]['u'], table[i]['v'], table[i]['x']);
+            }
+            Console.WriteLine();
         }
 
         public static void Print(double[,] array)
--- GausMethod/Program.cs
+++ GausMethod/Program.cs
@@ -8,18 +8,10 @@ namespace GausMethod
     {
         static void Main(string[] args)
         {
-            var reg = @"([\+-]?\d?)x(\d)";
+            var reg = @"([\+-]?\d?[.,]?\d?)x(\d)";
             Console.Write("n = ");
             int n = int.Parse(Console.ReadLine());
-            if (n == 0)
-            {
-                Counter.Count(new double[,] {
-                { 2, 1, 1, 1, 1 },
-                { 2, 2, 2, 3, -1},
-                { 4, 3, 3, 3, 1 },
-                { 6, 4, 5, 2, 4 } });
-                return;
-            }
+            
 
             var arr = new double[n, n + 1];
             for(var i = 0; i < n; i++)
@@ -31,6 +23,7 @@ namespace GausMethod
                     var e = t.Groups[1].Value;
                     switch (e)
                     {
+                        case "":
                         case "+":
                             arr[i, int.Parse(t.Groups[2].Value) - 1] = 1;
                             break;
@@ -42,8 +35,10 @@ namespace GausMethod
                             break;
                     }
                 }
-                arr[i, n] = Double.Parse(Regex.Match(line, @"=([\+-]?\d)").Groups[1].Value);
+                arr[i, n] = Double.Parse(Regex.Match(line, @"=([\+-]?\d?[.,]?\d)").Groups[1].Value);
             }
+            Counter.ThomasMethod(arr);
+            Console.WriteLine();
             Counter.Count(arr);
             //5x1+2x2+3x3-7x2=9
             //@"[\+-]?(/d)x/d"
